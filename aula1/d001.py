611c0530e90e76f7c3a5a94a54dd9f2c654ebb53
Patrick Cruz <145387737+cruzPatrick@users.noreply.github.com>
2024-04-11 12:14:31 -0300

    exercicios

---
+++ aula1/d001.py
@@ -0,0 +1,21 @@
+print('=============================DESAFIO 01=============================')
+
+nome = input('Qual o seu nome? ')
+
+print('Bem vindo,',nome)
+
+print('=============================DESAFIO 02=============================')
+
+dia = input('Qual o seu dia de nascimento? ')
+mes = input('Qual o seu mês de nascimento? ')
+ano = input('Qual o seu ano de nascimento? ')
+
+print(dia,'/',mes,'/',ano)
+print('você nasceu no dia', dia,'de' ,mes, 'de',ano,'. correto?')
+
+print('=============================DESAFIO 03=============================')
+
+num1 = input('escreva um número ')
+num2 = input('escreva outro número ')
+num3 = num1+num2
+print('a soma de',num1,'e',num2,'é',num3)
---
+++ aula1/ex001.py
@@ -0,0 +1,4 @@
+nome = input('Seu nome é? ')
+idade = input('Sua idade é? ')
+peso = input('Seu peso é? ')
+print(nome,idade, peso)
