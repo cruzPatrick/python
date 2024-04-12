bd74670ab89cec2197095f984b2308143400bdd7
Patrick Cruz <145387737+cruzPatrick@users.noreply.github.com>
2024-04-12 08:57:12 -0300

    algumas organizações dos arquivos

--- desafios/d001-003.py
+++ desafios/d001-003.py
@@ -12,11 +12,21 @@ ano = input('Qual o seu ano de nascimento? ')
 
 print(dia,'/',mes,'/',ano)
 print('você nasceu no dia', dia,'de' ,mes, 'de',ano,'. correto?')
+print('Seu ano é {}!'.format(ano))
 
 print('=============================DESAFIO 03=============================')
 
+n1=int(input('Escreva um numero: '))
+n2=int(input('Escreva outro numero: '))
+print('a soma dos numeros é {}'.format(n1+n2))
+#print('A soma entre', n1, 'e',n2,'vale:',n1+n2)
+print('a soma entre {} e {} vale {}'.format(n1,n2,n1+n2))
+
+'''
 num1 = input('escreva um número ')
 num2 = input('escreva outro número ')
 num3 = num1+num2
 print('a soma de',num1,'e',num2,'é',num3)
 print('deu ruim, mas vai ter a correção na próxima aula')
+
+'''
